8e3713be0213f43af6e9743844a1cb1472c7bc79
helinjiang <me@helinjiang.com>
2020-04-11 16:19:35 +0800

    feat: 使用 react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// import './App.less';
+
+export default class App extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>hello,world!</h1>
+            </div>
+        );
+    }
+}
